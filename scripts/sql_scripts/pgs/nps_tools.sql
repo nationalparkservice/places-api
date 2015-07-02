183fa71c316b94d492373a09a933868991f81440
Jim McAndrew <jim@loc8.us>
2015-07-02 11:39:29 -0600

    Update nps_tools.sql

--- scripts/sql_scripts/pgs/nps_tools.sql
+++ scripts/sql_scripts/pgs/nps_tools.sql
@@ -797,21 +797,18 @@ $BODY$
 
 --------------------------------
 -- Render a full changeset
-CREATE OR REPLACE FUNCTION public.o2p_render_changeset(bigint)
-  RETURNS boolean[] AS
-$BODY$
   DECLARE
     v_changeset_id ALIAS FOR $1;
     v_return_value boolean[];
   BEGIN
     SELECT array_agg(o2p_render_element(all_types.id, all_types.member_type)) FROM (
-      SELECT id, 'N'::character as member_type, changeset_id from api_nodes 
+      SELECT id, 'N'::character as member_type, changeset from api_nodes 
       UNION ALL
-      SELECT id, 'W'::character as member_type, changeset_id from api_ways
+      SELECT id, 'W'::character as member_type, changeset from api_ways
       UNION ALL
-      SELECT id, 'R'::character as member_type, changeset_id from api_relations
+      SELECT id, 'R'::character as member_type, changeset from api_relations
     ) all_types
-    WHERE all_types.changeset_id = v_changeset_id
+    WHERE all_types.changeset = v_changeset_id
     INTO v_return_value;
 
     RETURN v_return_value;
