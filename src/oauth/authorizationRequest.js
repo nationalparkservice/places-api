ca4398548ac470d00e5f3ce9974429ac7cb73553
Jim McAndrew <jim@loc8.us>
2015-07-31 10:50:26 -0600

    Use an array for auth

--- src/oauth/authorizationRequest.js
+++ src/oauth/authorizationRequest.js
@@ -1,18 +1,18 @@
 /* jshint camelcase: false */
-module.exports = function(config) {
-  return function(req, callback) {
+module.exports = function (config) {
+  return function (req, callback) {
     var tools = require('./tools')(config);
     var auths = tools.splitAuthHeader(req.headers.authorization);
-    tools.getAccessSecret(auths.oauth_token, function(tokens) {
+    tools.getAccessSecret(auths.oauth_token, function (tokens) {
       if (tokens) {
-        tools.getUserInfo(tokens.request_token, tokens.request_token_secret, auths.oauth_token, tokens.access_token_secret, function(e, username, userId) {
+        tools.getUserInfo(tokens.request_token, tokens.request_token_secret, auths.oauth_token, tokens.access_token_secret, function (e, username, userId) {
           // Verify the signature
-          var origSig = auths.oauth_signature,
-            requestedUrl = req.protocol + '://' + req.get('Host') + req.originalUrl,
-            compareSig = tools.osmOauth._getSignature(req.method, requestedUrl, tools.verification.baseString(auths), tokens.access_token_secret),
-            josmCompareSig = tools.josmOauth._getSignature(req.method, requestedUrl, tools.verification.baseString(auths), tokens.access_token_secret),
-            arcCompareSig = tools.arcOauth._getSignature(req.method, requestedUrl, tools.verification.baseString(auths), tokens.access_token_secret);
-          if (encodeURIComponent(compareSig) === origSig || encodeURIComponent(josmCompareSig) === origSig || encodeURIComponent(arcCompareSig) === origSig) {
+          var origSig = auths.oauth_signature;
+          var requestedUrl = req.protocol + '://' + req.get('Host') + req.originalUrl;
+          var compareSigs = tools.osmOauth._getSignatures.map(function (getSignature) {
+            return encodeURIComponent(getSignature(req.method, requestedUrl, tools.verification.baseString(auths), tokens.access_token_secret));
+          });
+          if (compareSigs.indexOf(origSig) > -1) {
             callback({
               'valid': !e,
               'userId': userId,
@@ -32,3 +32,4 @@ module.exports = function(config) {
     });
   };
 };
+
