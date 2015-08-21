0e05a638e6415b4e9290b82e59eb73b265eeb26f
Jim McAndrew <jim@loc8.us>
2015-08-21 11:18:44 -0600

    reformat for semistandard

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-var allowXSS = require('./src/allowXSS'),
-  bodyParser = require('body-parser'),
-  express = require('express'),
-  timeout = require('connect-timeout');
+var allowXSS = require('./src/allowXSS');
+var bodyParser = require('body-parser');
+var express = require('express');
+var timeout = require('connect-timeout');
 
-module.exports = function(config) {
-
+module.exports = function (config) {
   config = config || require('./config');
-  var apiData = require('./src/apis/data')(config),
-    apiXapi = require('./src/apis/xapi')(config),
-    oauth = require('./src/oauth/paths')(config);
+  var apiData = require('./src/apis/data')(config);
+  var apiXapi = require('./src/apis/xapi')(config);
+  var oauth = require('./src/oauth/paths')(config);
 
   return {
-    'routes': function() {
-      var router = express.Router(),
-        placesApp = require('./src/apiWrapper')(router, config);
+    'routes': function () {
+      var router = express.Router();
+      var placesApp = require('./src/apiWrapper')(router, config);
       // From http://wiki.openstreetmap.org/wiki/API_v0.6#General_information
       // Allow external webpages to read our JavaScript
       allowXSS(router);
 
       // API Calls
-      apiXapi.map(function(apiCall) {
+      apiXapi.map(function (apiCall) {
         placesApp.allow(apiCall.method, apiCall.path, '0.6', apiCall.format, apiCall.auth, apiCall.process);
       });
 
       // Data Calls
-      apiData.map(function(apiCall) {
+      apiData.map(function (apiCall) {
         placesApp.allow(apiCall.method, apiCall.path, 'data', apiCall.format, apiCall.auth, apiCall.process);
       });
 
       // Overall capabilities
-      placesApp.allow('GET', 'capabilities', null, null, null, function(req, res) {
+      placesApp.allow('GET', 'capabilities', null, null, null, function (req, res) {
         res.send({
           'api': config.capabilities
         });
@@ -39,11 +38,11 @@ module.exports = function(config) {
       router.use(timeout(config.capabilities.timeout.seconds + 's'));
       return router;
     },
-    'oauth': function() {
+    'oauth': function () {
       var router = express.Router();
 
       // Return the oauth calls
-      oauth.map(function(oauthCall) {
+      oauth.map(function (oauthCall) {
         router[(oauthCall.method).toLowerCase()](oauthCall.path, bodyParser.json(), oauthCall.process);
       });
 
