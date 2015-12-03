a7104f03848d19df068846421c9c79dc731c5a4e
Jim McAndrew <jim@loc8.us>
2015-12-03 16:53:17 -0500

    Add the summary view

--- scripts/sql_scripts/pgs/nps_tools.sql
+++ scripts/sql_scripts/pgs/nps_tools.sql
@@ -847,6 +847,107 @@ $BODY$
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
+  
+CREATE VIEW "summary" AS
+SELECT
+  "elements"."osm_id",
+  "elements"."name",
+  "elements"."superclass",
+  "elements"."class",
+  "elements"."type",
+  "elements"."tags"::json AS "tags",
+  "elements"."element_type" AS "element_type",
+  -- ST_AsGeoJson("elements"."the_geom") AS "geometry",
+  "elements"."tstamp",
+  "elements"."changeset_id",
+  "elements"."changeset_tags",
+  "elements"."tags"->'nps:unit_code' AS "unit_code",
+  "render_park_polys"."long_name" AS "unit_name",
+  "elements"."user_id" as "user_id",
+  "users"."name" AS "username"
+FROM (
+  SELECT
+    "nps_render_point"."osm_id",
+    "nps_render_point"."name",
+    "nps_render_point"."superclass",
+    "nps_render_point"."class",
+    "nps_render_point"."type",
+    "nps_render_point"."tags",
+    "nps_render_point"."the_geom",
+    'node' AS "element_type",
+    "nodes"."tstamp" As "tstamp",
+    "nodes"."changeset_id" as "changeset_id",
+    (select hstore(array_agg(k), array_agg(v)) from json_populate_recordset(null::new_hstore,(SELECT json_agg("result") FROM (SELECT "k", "v" FROM "api_changeset_tags" WHERE "api_changeset_tags"."changeset_id" = "nodes"."changeset_id") "result")))::json AS "changeset_tags",
+    "nodes"."user_id" as "user_id"
+  FROM
+    nps_render_point JOIN nodes ON nodes.id = osm_id
+  UNION ALL
+  SELECT
+    "nps_render_line"."osm_id",
+    "nps_render_line"."name",
+    "nps_render_line"."superclass",
+    "nps_render_line"."class",
+    "nps_render_line"."type",
+    "nps_render_line"."tags",
+    "nps_render_line"."the_geom",
+    'line' AS "element_type",
+    CASE WHEN
+      "nps_render_line"."osm_id" < 0 THEN "relations"."tstamp"
+    ELSE "ways"."tstamp"
+    END as "tstamp",
+    CASE WHEN
+      "nps_render_line"."osm_id" < 0 THEN "relations"."changeset_id"
+    ELSE "ways"."changeset_id"
+    END as "changeset_id",
+    CASE WHEN
+      "nps_render_line"."osm_id" < 0 THEN
+      (select hstore(array_agg(k), array_agg(v)) from json_populate_recordset(null::new_hstore,(SELECT json_agg("result") FROM (SELECT "k", "v" FROM "api_changeset_tags" WHERE "api_changeset_tags"."changeset_id" = "relations"."changeset_id") "result")))::json
+    ELSE
+      (select hstore(array_agg(k), array_agg(v)) from json_populate_recordset(null::new_hstore,(SELECT json_agg("result") FROM (SELECT "k", "v" FROM "api_changeset_tags" WHERE "api_changeset_tags"."changeset_id" = "ways"."changeset_id") "result")))::json
+    END AS "changeset_tags",
+    CASE WHEN
+      "nps_render_line"."osm_id" < 0 THEN "relations"."user_id"
+    ELSE "ways"."user_id"
+    END as "user_id"
+  FROM
+    "nps_render_line"
+      LEFT JOIN "ways" ON "nps_render_line"."osm_id" = "ways"."id"
+      LEFT JOIN "relations" ON "nps_render_line"."osm_id" * -1 = "relations"."id"
+  UNION ALL
+  SELECT
+    "nps_render_polygon"."osm_id",
+    "nps_render_polygon"."name",
+    "nps_render_polygon"."superclass",
+    "nps_render_polygon"."class",
+    "nps_render_polygon"."type",
+    "nps_render_polygon"."tags",
+    "nps_render_polygon"."the_geom",
+    'area' AS "element_type",
+    CASE WHEN
+      "nps_render_polygon"."osm_id" < 0 THEN "relations"."tstamp"
+    ELSE "ways"."tstamp"
+    END as "tstamp",
+    CASE WHEN
+      "nps_render_polygon"."osm_id" < 0 THEN "relations"."changeset_id"
+    ELSE "ways"."changeset_id"
+    END as "changeset_id",
+    CASE WHEN
+      "nps_render_polygon"."osm_id" < 0 THEN
+      (select hstore(array_agg(k), array_agg(v)) from json_populate_recordset(null::new_hstore,(SELECT json_agg("result") FROM (SELECT "k", "v" FROM "api_changeset_tags" WHERE "api_changeset_tags"."changeset_id" = "relations"."changeset_id") "result")))::json
+    ELSE
+      (select hstore(array_agg(k), array_agg(v)) from json_populate_recordset(null::new_hstore,(SELECT json_agg("result") FROM (SELECT "k", "v" FROM "api_changeset_tags" WHERE "api_changeset_tags"."changeset_id" = "ways"."changeset_id") "result")))::json
+    END AS "changeset_tags",
+   CASE WHEN
+      "nps_render_polygon"."osm_id" < 0 THEN "relations"."user_id"
+    ELSE "ways"."user_id"
+    END as "user_id"
+  FROM
+    "nps_render_polygon"
+      LEFT JOIN "ways" ON "nps_render_polygon"."osm_id" = "ways"."id"
+      LEFT JOIN "relations" ON "nps_render_polygon"."osm_id" * -1 = "relations"."id"
+) AS "elements"
+  LEFT JOIN "users" ON "elements"."user_id" = "users"."id"
+  LEFT JOIN "render_park_polys" ON lower("elements"."tags" -> 'nps:unit_code') = lower("render_park_polys"."unit_code");
 
 ---------------------------
 -- Foreign Data
