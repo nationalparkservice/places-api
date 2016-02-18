6fecfa9fa0ccb76f81ef9cb6b63cfb96b78fabfb
Jim McAndrew <jim@loc8.us>
2016-02-18 16:13:16 -0500

    Create update_tags.sql

---
+++ scripts/cleanup/update_tags.sql
@@ -0,0 +1,30 @@
+// Run this every time tags get changed
+// This will go through the database and find anything that has been rendered with the wrong tags and will update them
+// ALERT: This query may take up to an hour to run!
+
+SELECT
+  o2p_render_element(nps_render_point.osm_id, 'N')
+FROM
+ nps_render_point
+WHERE
+  o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'superclass' != nps_render_point.superclass OR
+  o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'class' != nps_render_point.class OR
+  o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'type' != nps_render_point.type
+UNION ALL
+SELECT
+  o2p_render_element(nps_render_line.osm_id, CASE WHEN osm_id >= 0 THEN 'W' ELSE 'R' END)
+FROM
+ nps_render_line
+WHERE
+  o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'superclass' != nps_render_line.superclass OR
+  o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'class' != nps_render_line.class OR
+  o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'type' != nps_render_line.type
+UNION ALL
+SELECT
+  o2p_render_element(nps_render_polygon.osm_id, CASE WHEN osm_id >= 0 THEN 'W' ELSE 'R' END)
+FROM
+ nps_render_polygon
+WHERE
+  o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'superclass' != nps_render_polygon.superclass OR
+  o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'class' != nps_render_polygon.class OR
+  o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'type' != nps_render_polygon.type;
