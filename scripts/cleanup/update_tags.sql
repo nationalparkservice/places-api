527e5b294a4f2b19d6b0043a1882aad2be858811
Jim McAndrew <jim@loc8.us>
2016-05-16 15:13:03 -0400

    Add layerIndex

--- scripts/cleanup/update_tags.sql
+++ scripts/cleanup/update_tags.sql
@@ -9,7 +9,8 @@ FROM
 WHERE
   o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'superclass' != nps_render_point.superclass OR
   o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'class' != nps_render_point.class OR
-  o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'type' != nps_render_point.type
+  o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'type' != nps_render_point.type OR
+  (o2p_get_preset(nps_render_point.tags, ARRAY['point'::text])::json->>'layerIndex')::integer != nps_render_point."z_order"
 UNION ALL
 SELECT
   o2p_render_element(nps_render_line.osm_id, CASE WHEN osm_id >= 0 THEN 'W' ELSE 'R' END)
@@ -18,7 +19,8 @@ FROM
 WHERE
   o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'superclass' != nps_render_line.superclass OR
   o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'class' != nps_render_line.class OR
-  o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'type' != nps_render_line.type
+  o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'type' != nps_render_line.type OR
+  (o2p_get_preset(nps_render_line.tags, ARRAY['line'::text])::json->>'layerIndex')::integer != nps_render_line."z_order"
 UNION ALL
 SELECT
   o2p_render_element(nps_render_polygon.osm_id, CASE WHEN osm_id >= 0 THEN 'W' ELSE 'R' END)
@@ -27,4 +29,5 @@ FROM
 WHERE
   o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'superclass' != nps_render_polygon.superclass OR
   o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'class' != nps_render_polygon.class OR
-  o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'type' != nps_render_polygon.type;
+  o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'type' != nps_render_polygon.type OR
+  (o2p_get_preset(nps_render_polygon.tags, ARRAY['line'::text])::json->>'layerIndex')::integer != nps_render_polygon."z_order";
